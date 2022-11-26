aa211cd60de1d28f84bf8d2fc0adecaf014fa56a
Michał Smolik <michal.smolik@nowatel.com>
2022-11-26 15:52:18 +0100

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '@/app.css'
+
+export const App = () => {
+	let a = 1
+
+	console.log(a)
+
+	a = 2
+
+	return (
+		<h1 className="text-2xl font-bold underline tex">
+			Hello world!
+		</h1>
+	)
+}
+
+export default App
